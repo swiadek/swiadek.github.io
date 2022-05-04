9e19995375cd150f3e14ec4231b53c6f2b269e74
Jakub Swiadek <jakub@jakubs-macbook-pro.local>
2022-05-04 22:54:33 +0000

    Major site re-do as a portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+lottie.loadAnimation({
+  container: document.getElementById("wave"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/wave.json"
+});
+
+lottie.loadAnimation({
+  container: document.getElementById("icons"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/icons.json"
+});
